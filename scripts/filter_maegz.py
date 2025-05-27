3ac44912cb5e46435c1d6b66c0cd77d6c91b42bf
juliavilmor <juliavilmor@gmail.com>
2025-05-27 10:57:50 +0200

    improve filter_maegz.py script

--- scripts/filter_maegz.py
+++ scripts/filter_maegz.py
@@ -12,35 +12,48 @@ def extract_ligand_best_poses(csv_file, maegz_files, output_dir, outname, virus)
     writer = struct.StructureWriter('%s/%s'%(output_dir,outname))
 
     for index, row in df.iterrows():
+        print('Processing row %d/%d: %s'%(index+1, len(df), row['id']))
+        
+        # Extract the outer round and the ids from the row
         outer = row['id'].split('_')[0]
-        print('Outer: ', outer)
+        ids = row['id'].split('_')[1:]
+        ids = '_'.join(ids)              # Title in csv file
+        ids = ids.split('-')[0]          # just in case there are duplicates      
+        gscores = row['gscore_%s'%virus] # Gscore in csv file
+        
         # Find the corresponding maegz file for outer round
         maegz_file = maegz_files[int(outer)-1]
-        print(maegz_file)
+        print('\t', maegz_file)
 
         m = struct.StructureReader(maegz_file)
+        
+        match_found = False  # Track if match is found
 
         for i, s in enumerate(m):
             if i == 0: continue # because the first structure is the receptor
             props = s.property.keys()
             title = s.title
-            title = title.split('.')[-1]    # Title in maegz file
-
-            ids = row['id'].split('_')[1:]
-            ids = '_'.join(ids)             # Title in csv file
-            gscores = row['gscore_%s'%virus]
-
-            if title != ids: continue
-            print(title, ids)
+            title = title.split('.')[-1]                 # Title in maegz file
+            title = title.split(',')[0]                  # just in case there are duplicates
+            st_gscore = s.property['r_i_docking_score']  # Gscore in maegz file
             
-            st_gscore = s.property['r_i_docking_score']
-            if round(st_gscore, 3) != round(gscores, 3): continue
-            print(round(gscores, 3), round(st_gscore, 3))
+            # If the title matches the id and the gscore matches
+            if title == ids and round(st_gscore, 5) == round(gscores, 5):
+                
+                print('\t', title, ids)
+                print('\t', round(gscores, 5), round(st_gscore, 5))
+            
+                # change maegz title
+                s.title = row['id'].split('-')[0]
 
-            # change maegz title
-            s.title = row['id']
-
-            writer.append(s)
+                # write the new maegz file
+                writer.append(s)
+                
+                match_found = True
+                break
+            
+        if not match_found:
+            print('WARNING: No match found for %s (%s) in %s'%(ids, gscores, maegz_file))
             
         m.close()
     writer.close()
@@ -56,10 +69,12 @@ if __name__ == '__main__':
     resdir = '/home/cactus/julia/gensim/selective_nocatalog_pretrained'
     virus = 'MERS' # Choose from: 'SARS2', 'SARS', 'MERS'
     target = '7eneC1' # Choose from: '7rnwA1', '2gx4A1', '7eneC1'
-    csv_file = '%s/results_filt.csv'%resdir
+    csv_file = '%s/results_filt_8_catalogues.csv'%resdir
     maegz_files = glob.glob('%s/glide_?/docking/%s_%s_pv*.maegz'%(resdir,virus, target))
     maegz_files.sort()
-    maegz_files.append('%s/glide_10/docking/%s_%s_pv.maegz'%(resdir, virus, target))
+    maegz_files2 = glob.glob('%s/glide_??/docking/%s_%s_pv.maegz'%(resdir, virus, target))
+    maegz_files2.sort()
+    maegz_files = maegz_files + maegz_files2
     outdir = resdir
     outname = '%s_ligands_filtered.maegz'%virus
 
